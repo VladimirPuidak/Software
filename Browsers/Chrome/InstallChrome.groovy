a3af9b07a181e9bd4c960d8ed57c61fe2788f738
Vladimir Puidak <skylikez@mail.ru>
2021-08-31 22:01:05 +0300

    Update InstallChrome.groovy

--- Browsers/Chrome/InstallChrome.groovy
+++ Browsers/Chrome/InstallChrome.groovy
@@ -11,7 +11,7 @@ pipeline {
     stages {
         stage("Download Git") {
             steps {
-                checkout scm
+                git checkout
             }
         }
         stage("Get hostname") {

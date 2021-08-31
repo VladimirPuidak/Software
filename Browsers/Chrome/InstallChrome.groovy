e34714a0828dddf77b7f58e5afd2a40c926f2148
Vladimir Puidak <skylikez@mail.ru>
2021-08-31 20:23:06 +0300

    Update InstallChrome.groovy

--- Browsers/Chrome/InstallChrome.groovy
+++ Browsers/Chrome/InstallChrome.groovy
@@ -1,1 +1,30 @@
-s
+#!groovy
+properties([
+	buildDiscarder(
+		logRotator(
+			artifactDaysToKeepStr: '',
+			artifactNumToKeepStr: '',
+			daysToKeepStr: '',
+			numToKeepStr: '100')
+	),
+	disableConcurrentBuilds()
+  timestamps()
+])
+
+pipeline {
+    agent { 
+        label 'WindowsClient'                                                                    
+        }
+    stages {
+        stage("Download Git") {
+            steps {
+                checkout scm
+            }
+        }
+        stage("Get hostname") {
+            steps {
+                powershell 'hostname'
+            }
+        }
+    }
+}
